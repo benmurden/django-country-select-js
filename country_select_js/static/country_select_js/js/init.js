e61b32ad4d8997e430e8a8543d90d48a25814c12
Benjamin Murden <ben.murden@gmail.com>
2016-07-12 16:09:25 +0900

    Init script for widget

---
+++ country_select_js/static/country_select_js/js/init.js
@@ -0,0 +1,34 @@
+(function($) {
+  var $el, options, $form, data,
+      cssClass = '.country-select-js',
+      forms = [],
+      inputs = $(cssClass);
+
+  inputs.each(function(i, el) {
+    $el = $(el);
+    data = $el.data();
+    options = {
+      initialCountry: "auto",
+      geoIpLookup: function(callback) {
+        $.get('http://ipinfo.io', function() {}, "jsonp").always(function(resp) {
+          var countryCode = (resp && resp.country) ? resp.country : "";
+          callback(countryCode);
+        });
+      }
+    };
+
+    $el.countrySelect(options)
+    .done(function() {
+      $form = $el.closest('form');
+      if (forms.indexOf($form) === -1) {
+        $form.submit(function(e) {
+          $form.find('input' + cssClass).val(function() {
+            return $(this).countrySelect("getSelectedCountryData").iso2;
+          });
+        });
+
+        forms.push($form);
+      }
+    });
+  });
+})(jQuery);
