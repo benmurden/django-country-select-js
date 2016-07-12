36e9e62e508592093bda73cdccc5e74fff3a4d9a
Benjamin Murden <ben.murden@gmail.com>
2016-07-12 16:08:56 +0900

    Basic widget layout.
    With preferred countries option.

---
+++ country_select_js/widgets.py
@@ -0,0 +1,24 @@
+import json
+from django import forms
+from django.utils.html import format_html
+
+
+class IntlTelInputWidget(forms.TextInput):
+
+    class Media:
+        css = {
+            'all': ('country_select_js/css/countrySelect.min.css',),
+        }
+        js = ('country_select_js/js/countrySelect.min.js', 'country_select_js/js/init.js')
+
+    def __init__(self, attrs=None, preferred_countries=['us', 'gb']):
+        final_attrs = {'class': 'country-select-js', 'size': '20'}
+        if attrs is not None:
+            final_attrs.update(attrs)
+
+        final_attrs['data-preferred-countries'] = json.dumps(preferred_countries)
+
+        super(IntlTelInputWidget, self).__init__(attrs=final_attrs)
+
+    def get_options(self):
+        return json.dumps(self.options)
