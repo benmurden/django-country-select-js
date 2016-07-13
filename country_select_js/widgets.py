dd9ad01984bf93bd61433014fb725edf68f87ea7
Benjamin Murden <ben.murden@gmail.com>
2016-07-13 11:08:22 +0900

    Expand size.

--- country_select_js/widgets.py
+++ country_select_js/widgets.py
@@ -11,7 +11,7 @@ class CountrySelectWidget(forms.TextInput):
         js = ('country_select_js/js/countrySelect.min.js', 'country_select_js/js/init.js')
 
     def __init__(self, attrs=None, preferred_countries=['us', 'gb']):
-        final_attrs = {'class': 'country-select-js', 'size': '20'}
+        final_attrs = {'class': 'country-select-js', 'size': '30'}
         if attrs is not None:
             final_attrs.update(attrs)
 
