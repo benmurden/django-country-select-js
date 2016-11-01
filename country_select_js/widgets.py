82dcaa4a51acbeb17b2e3e56b0ff5a9b2dc10462
Benjamin Murden <ben.murden@gmail.com>
2016-11-01 13:04:20 +0900

    Render hidden value along with select.

--- country_select_js/widgets.py
+++ country_select_js/widgets.py
@@ -10,8 +10,8 @@ class CountrySelectWidget(forms.TextInput):
         }
         js = ('country_select_js/js/countrySelect.min.js', 'country_select_js/js/init.js')
 
-    def __init__(self, attrs=None, preferred_countries=['us', 'gb'], default_code='us'):
-        final_attrs = {'class': 'country-select-js', 'size': '30'}
+    def __init__(self, attrs=None, preferred_countries=['us', 'gb'], default_code='us', auto_geo_ip=False):
+        final_attrs = {'size': '30'}
         if attrs is not None:
             final_attrs.update(attrs)
 
@@ -22,3 +22,24 @@ class CountrySelectWidget(forms.TextInput):
 
     def get_options(self):
         return json.dumps(self.options)
+
+    def render(self, name, value, attrs=None):
+        if value is None:
+            value = ''
+        final_attrs = self.build_attrs(attrs, name=name, size=2)
+        final_attrs['type'] = 'hidden'
+        if value != '':
+            final_attrs['value'] = force_text(self._format_value(value))
+
+        output = [format_html('<input{}>', flatatt(final_attrs))]
+        select = self.render_select(attrs)
+        output.append(select)
+        return mark_safe('\n'.join(output))
+
+    def render_select(attrs):
+        final_attrs = {'class': 'country-select-js', 'size': '30'}
+        if attrs is not None:
+            final_attrs.update(attrs)
+
+        output = [format_html('<input{}>', flatatt(final_attrs))]
+        return output
